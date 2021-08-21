393c309b3a05966e082b2d91530dcd0e03ba1399
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-21 13:55:01 +0300

    add input method to enable property bidning"

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -7,6 +7,7 @@ import { Quote } from '../quote';
   styleUrls: ['./quote-detail.component.css'],
 })
 export class QuoteDetailComponent implements OnInit {
+  @Input() quote: Quote;
   constructor() {}
 
   ngOnInit(): void {}
