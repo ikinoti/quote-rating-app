c14365ce9dbc8a5d8bfa6dbd051a60fc26a0ce83
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-21 13:11:42 +0300

    update our array to include quotes description

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,22 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    { id: 1, title: 'Believe' },
-    { id: 2, title: 'Communication' },
-    { id: 3, title: 'Dream' },
+    {
+      id: 1,
+      title: 'Believe',
+      quoteDesc: 'There is no better prayer than what you believe in',
+    },
+    {
+      id: 2,
+      title: 'Communication',
+      quoteDesc: 'The key is communication but the lock is trust',
+    },
+    {
+      id: 3,
+      title: 'Dream',
+      quoteDesc:
+        'You have to visit your future wwith your nmind, then walk towards it in reality',
+    },
   ];
   constructor() {}
 
