d72193e277e784c42018ed3fe9928144dc8dc5b5
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-21 13:57:31 +0300

    update type of quote

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -7,7 +7,7 @@ import { Quote } from '../quote';
   styleUrls: ['./quote-detail.component.css'],
 })
 export class QuoteDetailComponent implements OnInit {
-  @Input() quote: Quote;
+  @Input() quote: any;
   constructor() {}
 
   ngOnInit(): void {}
