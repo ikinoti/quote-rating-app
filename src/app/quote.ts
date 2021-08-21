6951c4bb06af2f3638cc342ca9d391edfb0b1eaf
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-21 11:59:15 +0300

    define a blueprint for our quote class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,4 +3,9 @@ import { stringify } from '@angular/compiler/src/util';
 export class Quote {
   id: number;
   title: string;
+
+  constructor(id: number, title: string) {
+    this.id = id;
+    this.title = title;
+  }
 }
