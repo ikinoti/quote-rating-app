19bb72ac829e53de32c2b437cd35b61e480e7f12
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-21 13:08:16 +0300

    and quote description on the class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,9 +3,11 @@ import { stringify } from '@angular/compiler/src/util';
 export class Quote {
   id: number;
   title: string;
+  quoteDesc: string;
 
-  constructor(id: number, title: string) {
+  constructor(id: number, title: string, quoteDesc: string) {
     this.id = id;
     this.title = title;
+    this.quoteDesc = quoteDesc;
   }
 }
