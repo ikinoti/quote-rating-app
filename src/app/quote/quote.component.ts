d1b72486570ee0f4bb767106cd485f20e0429cb6
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-22 16:45:41 +0300

    modify to accomodate function to calculate time duration

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,6 +12,10 @@ export class QuoteComponent implements OnInit {
       1,
       'Believe',
       'There is no better prayer than what you believe in',
+      'Prop',
+      'Tosh',
+      new Date(),
+      0,
       0,
       0,
       false
@@ -20,6 +24,10 @@ export class QuoteComponent implements OnInit {
       2,
       'Communication',
       'The key is communication but the lock is trust',
+      '3 wise men',
+      'Kakai',
+      new Date(),
+      0,
       0,
       0,
       false
@@ -28,11 +36,27 @@ export class QuoteComponent implements OnInit {
       3,
       'Dream',
       'You have to visit your future wwith your nmind, then walk towards it in reality',
+      'Wahenga',
+      'Manduku',
+      new Date(),
+      0,
       0,
       0,
       false
     ),
   ];
+
+  get quoteFunction() {
+    return this.genQuote.bind(this);
+  }
+
+  getQuote(author: string, title: string, quoteDesc: string, creater: string) {
+    let number = this.quotes.length + 1;
+    let date = new Date();
+    this.quotes.push(
+      new Quote(number, title, quoteDesc, author, creater, date, 0, 0, 0, false)
+    );
+  }
   badQuote(isBad: any, index: any) {
     if (isBad) {
       if (isBad) {
@@ -46,31 +70,48 @@ export class QuoteComponent implements OnInit {
     }
   }
 
-  // toLike(like: boolean, index: any) {
-  //   if (like) {
-  //     this.quotes[index].upvotes += 1;
-  //     this.getHighest();
-  //   } else {
-  //     this.quotes[index].downvotes += 1;
-  //   }
-  // }
-  // getHighest() {
-  //   let highest = 0;
-  //   let highVotedQuote: Quote;
-  //   for (let quote of this.quotes) {
-  //     if (quote.upvotes > highest) {
-  //       highest = quote.upvotes;
-  //       highVotedQuote = quote;
-  //     }
-  //     if (quote.id === highVotedQuote.id) {
-  //       quote.highest = true;
-  //     } else {
-  //       quote.highest = false;
-  //     }
-  //   }
-  // }
+  toLike(like: boolean, index: any) {
+    if (like) {
+      this.quotes[index].upvotes += 1;
+      this.getHighest();
+    } else {
+      this.quotes[index].downvotes += 1;
+    }
+  }
+  getHighest() {
+    let highest = 0;
+    let highVotedQuote: Quote;
+    for (let quote of this.quotes) {
+      if (quote.upvotes > highest) {
+        highest = quote.upvotes;
+        highVotedQuote = quote;
+      }
+      if (quote.id === highVotedQuote.id) {
+        quote.highest = true;
+      } else {
+        quote.highest = false;
+      }
+    }
+  }
 
-  constructor() {}
+  constructor() {
+    for (let quote of this.quotes) {
+      var time = function () {
+        let today: any;
+        setInterval(() => {
+          (today = new Date()).getTime();
+        }, 1000);
+        setInterval(() => {
+          dateDiffSeconds();
+        }, 1000);
+        var dateDiffSeconds = function () {
+          var timeDiff: number = (today - quote.dateCreated.getTime()) / 1000;
+          quote.age = timeDiff;
+        };
+      };
+      time();
+    }
+  }
 
   ngOnInit(): void {}
 }
