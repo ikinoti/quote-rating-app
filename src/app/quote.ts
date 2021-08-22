4bec2d0340445d1d0fc8b31029a75400520c54c9
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-22 08:51:48 +0300

    Changed how we creat quotes blueprint class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,13 +1,9 @@
 import { stringify } from '@angular/compiler/src/util';
 
 export class Quote {
-  id: number;
-  title: string;
-  quoteDesc: string;
-
-  constructor(id: number, title: string, quoteDesc: string) {
-    this.id = id;
-    this.title = title;
-    this.quoteDesc = quoteDesc;
-  }
+  constructor(
+    public id: number,
+    public title: string,
+    public quoteDesc: string
+  ) {}
 }
