8a84999d3be6939f6f4cfbb77b20299239d919d9
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-22 13:04:01 +0300

    Modified the delete function'

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,24 +11,65 @@ export class QuoteComponent implements OnInit {
     new Quote(
       1,
       'Believe',
-      'There is no better prayer than what you believe in'
+      'There is no better prayer than what you believe in',
+      0,
+      0,
+      false
     ),
     new Quote(
       2,
       'Communication',
-      'The key is communication but the lock is trust'
+      'The key is communication but the lock is trust',
+      0,
+      0,
+      false
     ),
     new Quote(
       3,
       'Dream',
-      'You have to visit your future wwith your nmind, then walk towards it in reality'
+      'You have to visit your future wwith your nmind, then walk towards it in reality',
+      0,
+      0,
+      false
     ),
   ];
   badQuote(isBad: any, index: any) {
     if (isBad) {
-      this.quotes.splice(index, 1);
+      if (isBad) {
+        let toDel = confirm(
+          `Are you sure you want to delete ${this.quotes[index].title}?`
+        );
+        if (toDel) {
+          this.quotes.splice(index, 1);
+        }
+      }
     }
   }
+
+  // toLike(like: boolean, index: any) {
+  //   if (like) {
+  //     this.quotes[index].upvotes += 1;
+  //     this.getHighest();
+  //   } else {
+  //     this.quotes[index].downvotes += 1;
+  //   }
+  // }
+  // getHighest() {
+  //   let highest = 0;
+  //   let highVotedQuote: Quote;
+  //   for (let quote of this.quotes) {
+  //     if (quote.upvotes > highest) {
+  //       highest = quote.upvotes;
+  //       highVotedQuote = quote;
+  //     }
+  //     if (quote.id === highVotedQuote.id) {
+  //       quote.highest = true;
+  //     } else {
+  //       quote.highest = false;
+  //     }
+  //   }
+  // }
+
   constructor() {}
 
   ngOnInit(): void {}
