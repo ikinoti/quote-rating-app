ac9d2864ac292bb9570dd32ef421cebc8ffb5f66
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-22 16:42:51 +0300

    modify to accomodate function to calculate time duration

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -8,11 +8,28 @@ import { Quote } from '../quote';
 })
 export class QuoteDetailComponent implements OnInit {
   @Input() quote: any;
+  @Input() quotes: any;
   @Output() isBad = new EventEmitter<boolean>();
+  @Output() qVote = new EventEmitter<boolean>();
 
-  quoteBad(bad: boolean) {
+  deleteQuote(bad: boolean) {
     this.isBad.emit(bad);
   }
+
+  like(likeValue: boolean) {
+    this.qVote.emit(likeValue);
+  }
+  quoteWithHighestVotes() {
+    for (var index = 0; index < this.quotes.length; index++) {
+      for (var i = 0; i < this.quotes.length; i++) {
+        if (this.quotes[index].upvotes > this.quotes[i].upvotes) {
+          this.quotes[index].highest = true;
+          this.quotes[i].highest = false;
+        }
+      }
+    }
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
