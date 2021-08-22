209f914be63a9b958bf7862beeefa8f22385203d
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-22 11:00:43 +0300

    add function to act on a quote with the highest vote

---
+++ src/app/highest.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, Input, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighest]',
+})
+export class HighestDirective {
+  @Input('appHighest') highest: boolean;
+
+  constructor(private elem: ElementRef) {}
+  @HostListener('change') ngOnChanges() {
+    if (this.highest) {
+      this.elem.nativeElement.style.backgroundColor = 'green';
+      this.elem.nativeElement.style.color = 'white';
+    } else {
+      this.elem.nativeElement.style.backgroundColor = 'white';
+      this.elem.nativeElement.style.color = 'black';
+    }
+  }
+}
