593d6fd68cf21d544f8914ca4d0ee285127c8dd4
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-22 09:01:10 +0300

    updated to make use of quote class constructor

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,22 +8,21 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    {
-      id: 1,
-      title: 'Believe',
-      quoteDesc: 'There is no better prayer than what you believe in',
-    },
-    {
-      id: 2,
-      title: 'Communication',
-      quoteDesc: 'The key is communication but the lock is trust',
-    },
-    {
-      id: 3,
-      title: 'Dream',
-      quoteDesc:
-        'You have to visit your future wwith your nmind, then walk towards it in reality',
-    },
+    new Quote(
+      1,
+      'Believe',
+      'There is no better prayer than what you believe in'
+    ),
+    new Quote(
+      2,
+      'Communication',
+      'The key is communication but the lock is trust'
+    ),
+    new Quote(
+      3,
+      'Dream',
+      'You have to visit your future wwith your nmind, then walk towards it in reality'
+    ),
   ];
   constructor() {}
 
