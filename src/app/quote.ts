6c4a8558a7aba9971fe77ababf3c43d0f747f8b6
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-22 16:13:15 +0300

    added more values in our blueprint class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,16 @@
-import { stringify } from '@angular/compiler/src/util';
+// import { stringify } from '@angular/compiler/src/util';
 
 export class Quote {
   constructor(
     public id: number,
     public title: string,
-    public quoteDesc: string
+    public quoteDesc: string,
+    public author: string,
+    public creater: string,
+    public dateCreated: Date,
+    public upvotes: number,
+    public downvotes: number,
+    public age: number,
+    public highest: boolean
   ) {}
 }
